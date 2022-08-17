320c14a3c3ff5be4d41d78655c46ec7307c061ae
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2022-08-17 23:30:48 +0200

    toggleterm

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,5 @@
 local options = {
-	backup = false,		             -- stop backing up the file
-	clipboard = 'unnamedplus',     -- system clipboard integration	
+	backup = false,		             -- stop backing up the file	
  	tabstop = 2,	         				 -- tab spans 2 spaces	
   relativenumber = true, 
 	number = true,
@@ -12,7 +11,7 @@ local options = {
 	splitbelow = true,             -- split horizontally will add tab on the bottom
 	splitright = true,             -- split vertically will add tab on the right
 	wrap = false,
-
+	hidden = false
 }
 for opt_name, opt_value in pairs(options) do
 	vim.opt[opt_name] = opt_value
@@ -20,4 +19,5 @@ end
 
 vim.cmd "set whichwrap+=<,>,[,],h,l"  -- `l` can now move to the next line
 vim.cmd [[set iskeyword+==]]
+vim.opt.clipboard:append("unnamedplus") -- to use in WSL2 install `scoop install win32yank` by powershell
 
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -1,7 +1,16 @@
 -- packer setup 
 return require('packer').startup(function(use)
 
-	use 'wbthomason/packer.nvim'
-	use 'neovim/nvim-lspconfig'
-	use 'folke/tokyonight.nvim'
+	use 'wbthomason/packer.nvim'  -- just a packer manager
+	use 'neovim/nvim-lspconfig'   -- lsp
+	use 'folke/tokyonight.nvim'   -- color scheme
+	use 'kyazdani42/nvim-web-devicons'
+	use { 
+		'nvim-lualine/lualine.nvim', 
+		requires = { 
+			'kyazdani42/nvim-web-devicons', 
+			opt = true }
+		}   -- status line
+	use {"akinsho/toggleterm.nvim", tag = 'v2.*', config = function()
+end}
  end)
