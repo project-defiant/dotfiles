05a5eb377dbd3d1bfe20131bf5a4e60ee4d95f69
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2022-08-17 20:44:00 +0200

    vim.options

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,3 +1,23 @@
-vim.opt.termguicolors = true
-vim.opt.cursorline = true
+local options = {
+	backup = false,		             -- stop backing up the file
+	clipboard = 'unnamedplus',     -- system clipboard integration	
+ 	tabstop = 2,	         				 -- tab spans 2 spaces	
+  relativenumber = true, 
+	number = true,
+	shiftwidth = 2,								 -- clicking tab moves by 2 spaces
+	guifont = "monospace:h17",
+	fileencoding = "utf-8",
+	mouse = 'a',
+	smartindent = true,
+	splitbelow = true,             -- split horizontally will add tab on the bottom
+	splitright = true,             -- split vertically will add tab on the right
+	wrap = false,
+
+}
+for opt_name, opt_value in pairs(options) do
+	vim.opt[opt_name] = opt_value
+end
+
+vim.cmd "set whichwrap+=<,>,[,],h,l"  -- `l` can now move to the next line
+vim.cmd [[set iskeyword+==]]
 
