cb3d1cca3fc93fc94e34ac7b9f894000ac3d5e5e
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2022-08-17 22:35:28 +0200

    tokyonight colorscheme

--- init.lua
+++ init.lua
@@ -1,4 +1,7 @@
 require("user.options")
+require("user.keymaps")
+require("user.plugins")
+require("user.plugin_config.colorscheme")
 
 
 
---
+++ lua/user/plugins.lua
@@ -0,0 +1,7 @@
+-- packer setup 
+return require('packer').startup(function(use)
+
+	use 'wbthomason/packer.nvim'
+	use 'neovim/nvim-lspconfig'
+	use 'folke/tokyonight.nvim'
+ end)
