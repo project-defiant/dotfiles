afdbb8ab57262c201a17c7e87e32f30801e12b81
PROJECT-DEFIANT <szymonszyszkowski@gmail.com>
2022-08-20 10:06:16 +0200

    minor fixes in toggleterm config

--- lua/user/plugin_config/terminal.lua
+++ lua/user/plugin_config/terminal.lua
@@ -4,8 +4,8 @@ if not status_ok then
 end
 
 toggleterm.setup {
-  size = 20,
-  open_mapping = [[<m-0>]],
+  size = 100,
+  open_mapping = [[<c-t>]],
   hide_numbers = true,
   shade_filetypes = {},
   shade_terminals = true,
@@ -13,7 +13,7 @@ toggleterm.setup {
   start_in_insert = true,
   insert_mappings = true,
   persist_size = true,
-  direction = "horizontal",
+  direction = "vertical",
   close_on_exit = true,
   shell = vim.o.shell,
   float_opts = {
@@ -26,27 +26,15 @@ toggleterm.setup {
   },
 }
 
--- function _G.set_terminal_keymaps()
---  local opts = { noremap = true }
---  vim.api.nvim_buf_set_keymap(0, 't', '<esc>', [[<C-\><C-n>]], opts)
---  vim.api.nvim_buf_set_keymap(0, "t", "jk", [[<C-\><C-n>]], opts)
---  vim.api.nvim_buf_set_keymap(0, "t", "<m-h>", [[<C-\><C-n><C-W>h]], opts)
---  vim.api.nvim_buf_set_keymap(0, "t", "<m-j>", [[<C-\><C-n><C-W>j]], opts)
---  vim.api.nvim_buf_set_keymap(0, "t", "<m-k>", [[<C-\><C-n><C-W>k]], opts)
---  vim.api.nvim_buf_set_keymap(0, "t", "<m-l>", [[<C-\><C-n><C-W>l]], opts)
--- end
-
--- vim.cmd "autocmd! TermOpen term://* lua set_terminal_keymaps()"
-
 
 function _G.set_terminal_keymaps()
   local opts = {buffer = 0}
   vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', 'ii', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<c-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<c-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<c-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<c-l>', [[<Cmd>wincmd l<CR>]], opts)
 end
 
 -- if you only want these mappings for toggle term use term://*toggleterm#* instead
