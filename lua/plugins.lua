b970716ed57f2c4d307e65b3caa8f20dfe453094
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2022-12-02 20:31:36 +0100

    markdown support

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -51,8 +51,10 @@ return require('packer').startup(function(use)
 	use("tpope/vim-commentary")
 	use { 'glepnir/dashboard-nvim' }
 	-- install without yarn or npm
-	use({ "iamcco/markdown-preview.nvim", run = "cd app && npm install",
-		setup = function() vim.g.mkdp_filetypes = { "markdown" } end, ft = { "markdown" }, })
+	use({
+		"iamcco/markdown-preview.nvim",
+		run = function() vim.fn["mkdp#util#install"]() end,
+	})
 	use("jose-elias-alvarez/null-ls.nvim")
 	use('jalvesaq/Nvim-R')
 end)
