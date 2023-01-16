7fcb7040760ec9b46172a03f09d2fe561713dce9
PROJECT-DEFIANT <szymon.szyszkowski@mnm.bio>
2023-01-16 11:59:03 +0100

    description to modules

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
-require("plugins")
-require("keymaps")
-require("options")
-require("style")
-require("rust_lsp")
-require("navigation")
-require("treesitter")
-require("statusbar")
-require("lsp")
-require("start")
-require("saga")
+require("plugins") -- plugins with packer
+require("keymaps") -- regular vim keymap
+require("options") -- regular vim options
+require("style") -- which style I  currently use
+require("rust_lsp") -- special lsp for rust lang
+require("navigation") -- nvim tree
+require("treesitter") -- syntax highlight & more
+require("statusbar") -- the status bar You see below
+require("lsp") -- language server protocol
+require("start") -- startpage
+require("saga") -- some additional def, hits etc
